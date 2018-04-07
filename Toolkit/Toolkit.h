84d25fd1b2780aa99be4cf6cfd6200b61c96140e
Dorin Danciu <dorin.danciu@me.com>
2018-04-07 20:42:13 +0300

    Initial Commit

---
+++ Toolkit/Toolkit.h
@@ -0,0 +1,19 @@
+//
+//  Toolkit.h
+//  Toolkit
+//
+//  Created by Dorin Danciu on 07/04/2018.
+//  Copyright © 2018 beilmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Toolkit.
+FOUNDATION_EXPORT double ToolkitVersionNumber;
+
+//! Project version string for Toolkit.
+FOUNDATION_EXPORT const unsigned char ToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toolkit/PublicHeader.h>
+
+
